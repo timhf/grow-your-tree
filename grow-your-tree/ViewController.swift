577dc64282221aeaf9dc80d38b620c09d86f93ac
thft <thotfilter@osdr.org>
2020-03-21 09:56:45 +0100

    Initial Commit

---
+++ grow-your-tree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grow-your-tree
+//
+//  Created by Tim Hotfilter on 21.03.20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ grow-your-treeTests/grow_your_treeTests.swift
@@ -0,0 +1,34 @@
+//
+//  grow_your_treeTests.swift
+//  grow-your-treeTests
+//
+//  Created by Tim Hotfilter on 21.03.20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import XCTest
+@testable import grow_your_tree
+
+class grow_your_treeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
